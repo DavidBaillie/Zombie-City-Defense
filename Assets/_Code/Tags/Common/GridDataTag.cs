2e50a2151a005f015780033652aab8a2baa99ad8
DavidBaillie <DavidlBaillie@gmail.com>
2023-04-08 15:48:36 -0400

    Added query support for getting grid points

--- Assets/_Code/Tags/Common/GridDataTag.cs
+++ Assets/_Code/Tags/Common/GridDataTag.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using Unity.Mathematics;
 using UnityEngine;
 
 namespace Game.Tags.Common
@@ -13,10 +14,57 @@ namespace Game.Tags.Common
         private Vector3[] _worldPositions = new Vector3[0];
         public Vector3[] WorldPositions { get => _worldPositions; }
 
+        /// <summary>
+        /// Overrides the current world data for the new input
+        /// </summary>
+        /// <param name="positions">All valid world positions</param>
         public void SetWorldPositions(IEnumerable<Vector3> positions)
         {
             _worldPositions = positions.ToArray();
             MakeDirty();
+        }
+
+        /// <summary>
+        /// Attempts to get the best world grid position within a specified distance of the source
+        /// </summary>
+        /// <param name="source">Source position to check from</param>
+        /// <param name="bestPosition">Best match to return through</param>
+        /// <param name="maxDistance">Max distance for a match</param>
+        /// <returns>If a match was found</returns>
+        public bool TryGetClosestGridPosition(Vector3 source, out Vector3 bestPosition, float maxDistance = 1f)
+        {
+            //Default data
+            bestPosition = Vector3.zero;
+            if (WorldPositions == null || WorldPositions.Length < 1)
+                return false;
+
+            //Tracking vars
+            Vector3? bestVector = null;
+            float bestDistance = float.MinValue;
+
+            //Check all coords
+            foreach (var position in _worldPositions)
+            {
+                float distance = Vector3.Distance(source, position);
+
+                if (distance < bestDistance)
+                {
+                    bestDistance = distance;
+                    bestVector = position;
+                }
+            }
+
+            //Return state
+            if (bestVector == null)
+            {
+                bestPosition = Vector3.zero;
+                return false;
+            }
+            else
+            {
+                bestPosition = bestVector.Value;
+                return true;
+            }
         }
     }
 }
