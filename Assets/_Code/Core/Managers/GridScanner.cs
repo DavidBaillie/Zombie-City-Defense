dcac0e619973905dae2d0805244a65841ee8368e
DavidBaillie <DavidlBaillie@gmail.com>
2023-04-07 23:23:09 -0400

    Created grid data classes

---
+++ Assets/_Code/Core/Managers/GridScanner.cs
@@ -0,0 +1,24 @@
+﻿using Game.Tags.Common;
+using Game.Utilities.BaseObjects;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace Game.Core.Managers
+{
+    public class GridScanner : AExtendedMonobehaviour
+    {
+        [SerializeField, Required]
+        private GridDataTag gridData = null;
+
+        [SerializeField, BoxGroup("Grid Size")]
+        private Vector2 minGridPosition = new(-100, -100);
+
+        [SerializeField, BoxGroup("Grid Size")]
+        private Vector2 maxGridPosition = new(100, 100);
+
+        [SerializeField, MinValue(0.001), BoxGroup("Grid Size")]
+        private float gridSize = 1;
+
+        
+    }
+}
---
+++ Assets/_Code/Tags/Common/GridDataTag.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game.Tags.Common
+{
+    [CreateAssetMenu(menuName = AssetMenuName + "Data/Grid Data", fileName = "Grid Data Tag")]
+    public class GridDataTag : ATag
+    {
+        [SerializeField]
+        private Vector2[] _worldPositions = new Vector2[0];
+        public Vector2[] WorldPositions { get => _worldPositions; }
+
+        public void SetWorldPositions(List<Vector2> positions) => _worldPositions = positions.ToArray();
+        public void SetWorldPositions(Vector2[] worldPositions) => _worldPositions = worldPositions;
+    }
+}
